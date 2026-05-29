75372b0372cbd520375a1b17b3366dd64a98e514
Haseeb Ahmed <haseeb.ahmed20035@gmail.com>
2026-05-30 01:37:21 +0500

    commit

--- config/mail.js
+++ config/mail.js
@@ -2,16 +2,28 @@ const nodemailer = require('nodemailer')
 require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for 587
+  family: 4, // force IPv4, avoids ENETUNREACH IPv6 issue
+
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS?.replace(/\s/g, ''),
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+
+  tls: {
+    rejectUnauthorized: false,
   },
 })
 
 transporter.verify((error, success) => {
   if (error) {
-    console.log('MAIL CONFIG ERROR:', error.message)
+    console.log('MAIL CONFIG ERROR:', error)
   } else {
     console.log('MAIL SERVER READY')
   }
